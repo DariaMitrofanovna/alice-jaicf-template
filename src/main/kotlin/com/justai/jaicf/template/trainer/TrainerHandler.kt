5029a0e70d3737a683fd4b1061ecf7b94f547eff
kissed <kissed.code@gmail.com>
2021-01-30 15:21:44 +0300

    commit

--- src/main/kotlin/com/justai/jaicf/template/trainer/TrainerHandler.kt
+++ src/main/kotlin/com/justai/jaicf/template/trainer/TrainerHandler.kt
@@ -3,6 +3,8 @@ package com.justai.jaicf.template.trainer
 import com.justai.jaicf.channel.yandexalice.api.AliceBotRequest
 import com.justai.jaicf.context.ActionContext
 import com.justai.jaicf.template.db.FileDb
+import java.time.LocalDate
+import java.time.LocalTime
 
 class TrainerHandler {
 
@@ -14,9 +16,10 @@ class TrainerHandler {
             FileDb.write(
                 userAppId,
                 buildString {
-                    append("type: ${ctx.request.type};")
-                    append("input: ${ctx.request.input};")
-                    appendln()
+                    append("time: ${LocalDate.now()}; ")
+                    append("type: ${ctx.request.type}; ")
+                    append("input: ${ctx.request.input}; ")
+                    append("intents: ${(ctx.request as? AliceBotRequest)?.request?.nlu?.intents?.entries?.joinToString { it.key + " - " + it.value.toString() }}; ")
                 }
             )
 
